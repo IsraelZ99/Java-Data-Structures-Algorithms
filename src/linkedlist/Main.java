e4a9ab7cc3ff6ea960df968251b63f265615600c
Israel Znitch Garcia Hernandez <israel.garcia@coppel.com>
2024-04-15 10:14:28 -0600

    Reestructuring the Main, to have methods to test every Linked List Method

--- src/linkedlist/Main.java
+++ src/linkedlist/Main.java
@@ -2,6 +2,28 @@ package linkedlist;
 
 public class Main {
     public static void main(String[] args) {
+        testAppend();
+        testRemoveLast();
+    }
+
+    public static void testPrepend() {
+
+    }
+
+    public static void testAppend() {
+        LinkedList myLinkedList = new LinkedList(1);
+        myLinkedList.append(2);
+
+        System.out.println("\n*** Print Linked List ***");
+        myLinkedList.printLIst();
+
+        System.out.println();
+        myLinkedList.getHead();
+        myLinkedList.getTail();
+        myLinkedList.getLength();
+    }
+
+    public static void testRemoveLast() {
         LinkedList myLinkedList = new LinkedList(1);
         myLinkedList.append(2);
 
@@ -13,7 +35,7 @@ public class Main {
         myLinkedList.getTail();
         myLinkedList.getLength();
 
-        System.out.println("\n*** Remove the last items ***");
+        System.out.println("\n*** Removing the last items ***");
         // (2) items - Return 2 Nodes
         System.out.println(myLinkedList.removeLast().value);
         // (1) items - Return 1 Node
@@ -21,4 +43,5 @@ public class Main {
         // (0) items - Return null
         System.out.println(myLinkedList.removeLast());
     }
+
 }
