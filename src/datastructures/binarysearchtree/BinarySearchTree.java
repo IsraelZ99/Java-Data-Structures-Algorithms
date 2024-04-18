360d918ed71b65fd2e01d6aff0e6c5c734c9d51d
Israel Znitch Garcia Hernandez <israel.garcia@coppel.com>
2024-04-18 11:23:26 -0600

    Binary Search Tree: Adding insert method,and testing method

--- src/datastructures/binarysearchtree/BinarySearchTree.java
+++ src/datastructures/binarysearchtree/BinarySearchTree.java
@@ -4,4 +4,51 @@ class BinarySearchTree {
     //The root node of the Tree
     // This normally is private
     Node root;
+
+    /**
+     * Insert new node on to the Tree
+     * @param value it's the value/number og the node
+     * @return If the new node was insert or not
+     */
+    public boolean insert(int value) {
+        Node newNode = new Node(value);
+        // Check if the tree is empty
+        if (root == null) {
+            root = newNode;
+            return true;
+        }
+        // Set temp to point to the root node
+        Node temp = root;
+        while (true) {
+            // This check if the new node has the same value of the temp node
+            // temp node is changing
+            if (newNode.value == temp.value) return false;
+            // Check if new node is less than temp, if it's correct the new node
+            // is going to be on the left side
+            if (newNode.value < temp.value) {
+                // Check if the spot is open, that means is not node on this position
+                if (temp.left == null) {
+                    // Link the new Node with the left arrow of temp node
+                    // In other words, set left property to point to new node
+                    temp.left = newNode;
+                    return true;
+                }
+                // If the spot isn't open, set temp to point to the left side of the node
+                temp = temp.left;
+            }
+            // If the new node is greater than temp the new node is going to be
+            // on the right side
+            else {
+                // Check if the spot is open, that means is not node on this position
+                if (temp.right == null) {
+                    // Link the new Node with the right arrow of temp node
+                    // In other words, set right property to point to new node
+                    temp.right = newNode;
+                    return true;
+                }
+                // If the spot isn't open, set temp to point to the right side of the node
+                temp = temp.right;
+            }
+        }
+    }
 }
--- src/datastructures/binarysearchtree/Main.java
+++ src/datastructures/binarysearchtree/Main.java
@@ -2,7 +2,26 @@ package datastructures.binarysearchtree;
 
 public class Main {
     public static void main(String[] args) {
-        testConstructor();
+        // testConstructor();
+        testInsert();
+    }
+
+    public static void testInsert() {
+        BinarySearchTree myBST = new BinarySearchTree();
+
+        myBST.insert(47);
+        myBST.insert(21);
+        myBST.insert(76);
+        myBST.insert(18);
+        myBST.insert(52);
+        myBST.insert(82);
+
+        System.out.println("Inserting 27 node ...");
+        System.out.println(myBST.insert(27));
+
+        System.out.println("\nCheck if the 27 node was insert on the correct side");
+        System.out.println("root => left side => right side");
+        System.out.println(myBST.root.left.right.value);
     }
 
     public static void testConstructor() {
