b04c562ca45b36304ef652a3a686515fa0facef6
Israel Znitch Garcia Hernandez <israel.garcia@coppel.com>
2024-04-23 13:47:09 -0600

    Recursion: Add the factorial method like an example, and testing method

---
+++ src/datastructures/recursion/factorial/Main.java
@@ -0,0 +1,20 @@
+package datastructures.recursion.factorial;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("*** Factorial ***");
+        System.out.println(factorial(4));
+    }
+
+    /**
+     * Method to get factorial of number
+     * @param n The number to get it's factorial
+     * @return The factorial number
+     */
+    public static int factorial(int n) {
+        // This is the base case
+        if (n == 1) return 1;
+        // This is the recursive case
+        return n * factorial(n - 1);
+    }
+}
